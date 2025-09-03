3c81ce82622d811caf75426bde937aa4661a35c7
Anis-Dangol <anis.dangol121@gmail.com>
2025-09-03 20:34:17 +0545

    6 - Sidebar components refactor according to react

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,18 @@
-const Input = () => {
-  return <div>Input</div>;
+const Input = ({ handleChange, value, title, name, color }) => {
+  return (
+    <div>
+      <label className="sidebar-label-container">
+        <input
+          onChange={handleChange}
+          type="radio"
+          value={{ value }}
+          name={name}
+        />
+        <span className="checkmark" style={{ backgroundColor: color }}></span>
+        {title}
+      </label>
+    </div>
+  );
 };
 
 export default Input;
